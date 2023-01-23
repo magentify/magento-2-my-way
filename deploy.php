e9cc486e31fad266af0a6dfe4898c9370f2295f3
Giacomo Moscardini <gmoscardini@webgriffe.com>
2023-01-23 17:39:02 +0100

    [WIP] prepare db:pull and media:pull recipe

--- deploy.php
+++ deploy.php
@@ -2,38 +2,7 @@
 
 namespace Deployer;
 
-require 'recipe/magento2.php';
-
-import(__DIR__ . '/deployer-host.yml');
-
-set('default_timeout', 1200);
-set('keep_releases', 3);
-add('shared_files', ['pub/.htaccess', 'pub/robots.txt', 'app/etc/env.php', 'var/.maintenance.ip', 'cron.sh']);
-set('artifact_file', 'artifact.tar.gz');
-set('artifact_dir', '.');
-
-desc('Remove jobs of bin/magento queue:consumers:start');
-task('magento:remove-append-consumers', function () {
-    run("ps -ef | grep 'bin/magento queue:consumers:start' | grep -v grep | awk '{print $2}' | xargs -r kill -9");
-});
-
-desc('Deploy your project');
-task('deploy', [
-    'deploy:info',
-    'deploy:setup',
-    'deploy:lock',
-    'deploy:release',
-    'artifact:upload',
-    'artifact:extract',
-    'deploy:shared',
-    'deploy:clear_paths',
-    'magento:upgrade:db',
-    'magento:remove-append-consumers',
-    'magento:cache:flush',
-    'deploy:symlink',
-    'deploy:unlock',
-    'deploy:cleanup',
-    'deploy:success',
-]);
-
-after('deploy:failed', 'deploy:unlock');
+import(__DIR__ . '/deployer/host.yml');
+import(__DIR__ . '/deployer/deploy.php');
+import(__DIR__ . '/deployer/dp_pull.php');
+import(__DIR__ . '/deployer/media_pull.php');
---
+++ deployer/deploy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/magento2.php';
+
+set('default_timeout', 1200);
+set('keep_releases', 3);
+add('shared_files', ['pub/.htaccess', 'pub/robots.txt', 'app/etc/env.php', 'var/.maintenance.ip', 'cron.sh']);
+set('artifact_file', 'artifact.tar.gz');
+set('artifact_dir', '.');
+
+desc('Remove jobs of bin/magento queue:consumers:start');
+task('magento:remove-append-consumers', function () {
+    run("ps -ef | grep 'bin/magento queue:consumers:start' | grep -v grep | awk '{print $2}' | xargs -r kill -9");
+});
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'artifact:upload',
+    'artifact:extract',
+    'deploy:shared',
+    'deploy:clear_paths',
+    'magento:upgrade:db',
+    'magento:remove-append-consumers',
+    'magento:cache:flush',
+    'deploy:symlink',
+    'deploy:unlock',
+    'deploy:cleanup',
+    'deploy:success',
+]);
+
+after('deploy:failed', 'deploy:unlock');
